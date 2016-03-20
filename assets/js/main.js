3e2d8a4f105743a9f19619f3625d60eee14379fc
jsonkuan <jsonkuan@gmail.com>
2016-03-20 16:37:12 +0000

    updated homepage content

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,18 +25,6 @@ setTimeout(function(){
 setTimeout(function(){
         $(".element2").css("display", "inherit");
         $(".element2").typed({
-            strings: ["/blog/"],
-            typeSpeed: 20, // typing speed
-            backDelay: 750, // pause before backspacing
-            loop: false, // loop on or off (true or false)
-            loopCount: false, // number of loops, false = infinite
-            callback: function(){ } // call function after typing is done
-        });
-    }, 2500);
-
-setTimeout(function(){
-        $(".element3").css("display", "inherit");
-        $(".element3").typed({
             strings: ["/coming soon.../"],
             typeSpeed: 20, // typing speed
             backDelay: 750, // pause before backspacing
@@ -47,8 +35,8 @@ setTimeout(function(){
     }, 3000);
 
 setTimeout(function(){
-        $(".element4").css("display", "inherit");
-        $(".element4").typed({
+        $(".element3").css("display", "inherit");
+        $(".element3").typed({
             strings: ["resume.pdf"],
             typeSpeed: 20, // typing speed
             backDelay: 750, // pause before backspacing
@@ -59,8 +47,8 @@ setTimeout(function(){
     }, 3500);
 
 setTimeout(function(){
-        $(".element5").css("display", "inherit");
-        $(".element5").typed({
+        $(".element4").css("display", "inherit");
+        $(".element4").typed({
             strings: ["@jsonkuan"],
             typeSpeed: 20, // typing speed
             backDelay: 750, // pause before backspacing
@@ -71,9 +59,9 @@ setTimeout(function(){
     }, 4000);
 
 	setTimeout(function(){
-        $(".element6").css("display", "inherit");
-        $(".element6").typed({
-            strings: ["\"iOS dev\",\n\"wine\",\n\"basenji's\"\n"],
+        $(".element5").css("display", "inherit");
+        $(".element5").typed({
+            strings: ["iOS dev"],
             typeSpeed: 1, // typing speed
             backDelay: 750, // pause before backspacing
             loop: false, // loop on or off (true or false)
@@ -82,3 +70,26 @@ setTimeout(function(){
         });
     }, 5000);
 
+	setTimeout(function(){
+        $(".element6").css("display", "inherit");
+        $(".element6").typed({
+            strings: ["wine"],
+            typeSpeed: 1, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 5000);
+	setTimeout(function(){
+        $(".element7").css("display", "inherit");
+        $(".element7").typed({
+            strings: ["basenji's"],
+            typeSpeed: 1, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 5000);
+	
