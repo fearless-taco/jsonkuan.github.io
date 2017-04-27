0711a849cd8d052397359efa77d4738b639e6808
jas0n- <jsonkuan@gmail.com>
2017-04-27 13:44:22 +0200

    Formatted(centered) & updated homepage

--- script.js
+++ script.js
@@ -4,36 +4,42 @@ app.config(function ($routeProvider) {
     $routeProvider.when('/projects', {
         controller: 'ProjectsController',
         templateUrl: 'partials/projects.html'
+    }).when('/resume', {
+        controller: 'CVController',
+        templateUrl: 'partials/resume.html'
     }).when('/fortunummer', {
         controller: 'FortunummerController',
         templateUrl: 'partials/fortunummer.html'
-    }).when('/resume', {
-        controller: 'HomeController',
-        templateUrl: 'partials/resume.html'
     }).otherwise({
         controller: 'HomeController',
         templateUrl: 'partials/home.html'
     });
 });
 
-app.directive('jsonkHelloWorld', function() {
-    return {
-        template: "Directive {{x}}"
-    };
-});
-
-app.controller('ProjectsController', function($scope, $rootScope) {
-    $scope.x = 'Hell0 Xorld!';
-    $scope.y = 'Hell0 Yorld!';
-});
-
-app.controller('FortunummerController', function($scope, $rootScope) {
-    $scope.x = 'bXe!';
-    $scope.y = 'bYe!';
-});
-
+/* CONTROLLERS */
 app.controller('HomeController', function($scope) {
     $scope.x = 'H0mex!';
     $scope.y = 'H0mey!';
 });
+
+app.controller('ProjectsController', function($scope, $rootScope) {
+    //Implement this...
+});
+
+app.controller('CVController', function($scope, $rootScope) {
+    //Implement this...
+});
+
+app.controller('FortunummerController', function($scope, $rootScope) {
+    //Implement this...
+});
+
+
+/* DIRECTIVES */
+app.directive('jsonkHelloWorld', function() {
+    return {
+        template: "Directive: Hello {{x}}!"
+    };
+});
+
 
