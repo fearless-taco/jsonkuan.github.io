9ce6fed6df2211e3cb29451226d3caea31ede04a
jas0n- <jsonkuan@gmail.com>
2017-04-28 17:27:02 +0200

    Integrated angular-typed-js library

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute','angularJSTypedEffect']);
 
 app.config(function ($routeProvider) {
     $routeProvider.when('/projects', {
